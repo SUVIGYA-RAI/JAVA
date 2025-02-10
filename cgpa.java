44fa87c780adfa5920640de59e920377de2ab6ca
Suvigya Rai <rai.suvigya3@gmail.com>
2025-02-10 19:52:11 +0530

    CGPA calculator

---
+++ cgpa.java
@@ -0,0 +1,9 @@
+public class cgpa {
+    public static void main(String[] args) {
+        float subject1 = 45;
+        float subject2 = 95;
+        float subject3 = 84;
+        float cgpa = (subject1 + subject2 + subject3)/30;
+        System.out.println(cgpa);
+    }
+}
