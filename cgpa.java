ff6d70171c9926c282c59f2880f63ac4d51f70fa
Suvigya Rai <rai.suvigya3@gmail.com>
2025-02-11 23:58:08 +0530

    CGPA calculator

---
+++ cgpa.java
@@ -0,0 +1,14 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class cgpa {
+    public cgpa() {
+    }
+ 
+    public static void main(String[] var0) {
+       float var1 = 45.0F;
+       float var2 = 95.0F;
+       float var3 = 84.0F;
+       float var4 = (var1 + var2 + var3) / 30.0F;
+       System.out.println(var4);
+    }
+ }
+ 
