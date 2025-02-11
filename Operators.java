36046f30eedaca52b90d86f7d12133f9fe8f9a73
Suvigya Rai <rai.suvigya3@gmail.com>
2025-02-11 23:59:35 +0530

    operator Types

---
+++ Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6 + a;
+        int c = 6 - a;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
