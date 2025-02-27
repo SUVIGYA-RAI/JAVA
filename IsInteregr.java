0c911dad4f0435b9b91e8e9e08bac356f48d7fea
Suvigya Rai <rai.suvigya3@gmail.com>
2025-02-28 01:51:56 +0530

    java codes

--- IsInteregr.java
+++ IsInteregr.java
@@ -3,8 +3,9 @@ public class IsInteregr {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        boolean a = sc.hasNextInt();
-        if (a==true) System.out.println("Number is integer.");
-        else System.out.println("Number is not an integer.");
+        System.out.println(sc.hasNextInt());
+        // boolean a = sc.hasNextInt();
+        // if (a==true) System.out.println("Number is integer.");
+        // else System.out.println("Number is not an integer.");
     }
 }
---
+++ Strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Strings {
+    public static void main(String[] args){
+        // String name = new String ("Suvigya");
+        // System.out.print("My name is: ");
+        // System.out.print(name);
+
+        // int a = 6;
+        // float b = 5.643f;
+        // System.out.printf("The value of a is %d and value of b is %f",a,b);
+
+        Scanner sc = new Scanner (System.in);
+        // String st = sc.next();
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
