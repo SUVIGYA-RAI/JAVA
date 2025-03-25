08528a776db59e3f4441aaa21fef9189f02e23d1
Suvigya Rai <rai.suvigya3@gmail.com>
2025-03-25 20:24:17 +0530

    string

--- StringMethod.java
+++ StringMethod.java
@@ -4,5 +4,24 @@ public class StringMethod {
         int value = a.length();
         System.out.println(a);
         System.out.println(value);
+
+        String ustring = a.toUpperCase();
+        System.out.println(ustring);
+    
+        String lstring = a.toLowerCase();
+        System.out.println(lstring);
+
+        String nonTrimmedString = "        Suvigya        ";
+        System.out.println(nonTrimmedString);
+        System.out.println(nonTrimmedString.trim());
+
+        System.out.println(a.substring(4)); 
+        System.out.println(a.substring(0,4));
+
+        // System.out.println(a.replace('u', 'a'));
+        System.out.println(a.replace("vigya", "man"));
+
+        System.out.println(a.charAt(2));
     }
 }
+
